45d5b710a4ecc5c35d050366eedd920f015484e6
egoekalp <erditaner.goekalp@eqs.com>
2022-12-30 23:47:43 +0100

    chore(experiments): event listener
    add event listeners for header clicks

--- angular-v12/src/app/change-detection-logger/change-detection-logger.ts
+++ angular-v12/src/app/change-detection-logger/change-detection-logger.ts
@@ -1,9 +1,12 @@
-export abstract class ChangeDetectionLogger {
+import { AfterViewInit, Directive } from '@angular/core';
+
+@Directive()
+export abstract class ChangeDetectionLogger implements AfterViewInit {
     abstract name: string;
 
     abstract parent: string | null;
 
-    protected readonly changeDetectionCounterKey: symbol = Symbol('change_detection_logger');
+    protected readonly changeDetectionCounterKey = Symbol('change_detection_logger');
 
     constructor() {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,9 +15,25 @@ export abstract class ChangeDetectionLogger {
 
     get log(): string {
         // eslint-disable-next-line no-console
-        console.log(`[${this.name}]-[${this.parent}]: ${this.increaseAndGetCount()}`);
+        console.log(`${this.logHeader}: ${this.increaseAndGetCount()}`);
 
         return '';
+    }
+
+    protected get logHeader(): string {
+        return `[${this.name}]-[${this.parent}]`;
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            // eslint-disable-next-line no-console
+            console.log(`${this.logHeader}: afterViewInit - setTimeout() resolved`);
+        }, 2000);
+    }
+
+    onClickHandler(event: MouseEvent): void {
+        // eslint-disable-next-line no-console
+        console.log(`${this.logHeader} - click handler executed`);
     }
 
     protected increaseAndGetCount(): number {
